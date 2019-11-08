6672984b0f98790a1593dd38a632f14bc5f6ce0a
Alexis Michaltsis <alexis.michaltsis@rl-institut.de>
2019-11-08 15:33:03 +0100

    Test that in JSON schemas the correct $schema and $id is set

--- tests/metadata/metaschema/draft07/test_schema.py
+++ tests/metadata/metaschema/draft07/test_schema.py
@@ -1,2 +1,15 @@
 def test_if_schema_json_loads_successfully():
     from metadata.metaschema.draft07.schema import METADATA_METASCHEMA_DRAFT07_SCHEMA
+
+
+def test_if_schema_json_has_correct_schema_and_id_set():
+    from metadata.metaschema.draft07.schema import METADATA_METASCHEMA_DRAFT07_SCHEMA
+
+    assert (
+        METADATA_METASCHEMA_DRAFT07_SCHEMA["$schema"]
+        == "http://json-schema.org/draft-07/schema#"
+    )
+    assert (
+        METADATA_METASCHEMA_DRAFT07_SCHEMA["$id"]
+        == "http://json-schema.org/draft-07/schema#"
+    )
--- tests/metadata/v140/test_schema.py
+++ tests/metadata/v140/test_schema.py
@@ -1,2 +1,12 @@
 def test_if_schema_json_loads_successfully():
     from metadata.v140.schema import METADATA_V140_SCHEMA
+
+
+def test_if_schema_json_has_correct_schema_and_id_set():
+    from metadata.v140.schema import METADATA_V140_SCHEMA
+
+    assert METADATA_V140_SCHEMA["$schema"] == "http://json-schema.org/draft-07/schema#"
+    assert (
+        METADATA_V140_SCHEMA["$id"]
+        == "https://raw.githubusercontent.com/OpenEnergyPlatform/metadata/master/metadata/v140/schema.json"
+    )
