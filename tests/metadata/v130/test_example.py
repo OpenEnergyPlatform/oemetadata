a472ee91fe0e14bd4e8b5258b1596da2b02677bf
Alexis Michaltsis <alexis.michaltsis@rl-institut.de>
2019-11-08 14:52:00 +0100

    Make tests more cohesive by putting imports into tests

--- tests/metadata/v130/test_example.py
+++ tests/metadata/v130/test_example.py
@@ -1,11 +1,9 @@
-import jsonschema
-
-
 def test_if_example_json_loads_successfully():
     from metadata.v130.example import METADATA_V130_EXAMPLE
 
 
 def test_example_against_schema_which_should_succeed():
+    import jsonschema
     from metadata.v130.example import METADATA_V130_EXAMPLE
     from metadata.v130.schema import METADATA_V130_SCHEMA
 
--- tests/metadata/v140/test_example.py
+++ tests/metadata/v140/test_example.py
@@ -1,11 +1,9 @@
-import jsonschema
-
-
 def test_if_example_json_loads_successfully():
     from metadata.v140.example import METADATA_V140_EXAMPLE
 
 
 def test_example_against_schema_which_should_succeed():
+    import jsonschema
     from metadata.v140.example import METADATA_V140_EXAMPLE
     from metadata.v140.schema import METADATA_V140_SCHEMA
 
