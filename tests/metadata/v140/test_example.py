3205362f24cdbac69e86d12c96555a5e2d459565
Alexis Michaltsis <alexis.michaltsis@rl-institut.de>
2019-11-08 14:44:20 +0100

    Put example against schema tests into example test files

--- tests/metadata/v140/test_example.py
+++ tests/metadata/v140/test_example.py
@@ -1,2 +1,12 @@
+import jsonschema
+
+
 def test_if_example_json_loads_successfully():
     from metadata.v140.example import METADATA_V140_EXAMPLE
+
+
+def test_example_against_schema_which_should_succeed():
+    from metadata.v140.example import METADATA_V140_EXAMPLE
+    from metadata.v140.schema import METADATA_V140_SCHEMA
+
+    assert jsonschema.validate(METADATA_V140_EXAMPLE, METADATA_V140_SCHEMA) == None
