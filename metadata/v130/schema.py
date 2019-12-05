c9ffa6261a9c2c05df6f986c740f6074cde8edfc
Alexis Michaltsis <a.michaltsis@gmail.com>
2019-12-05 16:17:00 +0100

    Read files in binary mode #17

--- metadata/v130/schema.py
+++ metadata/v130/schema.py
@@ -1,5 +1,5 @@
 import json
 import os
 
-with open(os.path.join(os.path.dirname(__file__), "schema.json"), "r") as f:
+with open(os.path.join(os.path.dirname(__file__), "schema.json"), "rb") as f:
     METADATA_V130_SCHEMA = json.loads(f.read())
--- metadata/v140/template.py
+++ metadata/v140/template.py
@@ -1,5 +1,5 @@
 import json
 import os
 
-with open(os.path.join(os.path.dirname(__file__), "template.json"), "r") as f:
+with open(os.path.join(os.path.dirname(__file__), "template.json"), "rb") as f:
     METADATA_V140_TEMPLATE = json.loads(f.read())
