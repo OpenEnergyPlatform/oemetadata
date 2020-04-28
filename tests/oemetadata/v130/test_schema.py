2d75630fffb925789efe1151fe304e56866e961b
jh-RLI <jonas.huber@rl-institut.de>
2020-04-28 20:33:20 +0200

    update id and schema url, fix ci error

--- tests/oemetadata/v130/test_schema.py
+++ tests/oemetadata/v130/test_schema.py
@@ -4,12 +4,14 @@ def test_if_schema_json_loads_successfully():
 
 def test_if_schema_json_has_correct_schema_and_id_set():
     from oemetadata.v130.schema import OEMETADATA_V130_SCHEMA
+    import string
 
-    assert OEMETADATA_V130_SCHEMA["$schema"] == "http://json-schema.org/draft-07/schema#"
-    assert (
-        OEMETADATA_V130_SCHEMA["$id"]
-        == "https://raw.githubusercontent.com/OpenEnergyPlatform/oemetadata/master/oemetadata/v130/schema.json"
-    )
+    def get_string(s):
+        return string.printable + s + string.printable
+
+    assert get_string(OEMETADATA_V130_SCHEMA["$schema"]) == get_string('http://json-schema.org/draft-07/schema#')
+
+    assert get_string(OEMETADATA_V130_SCHEMA["$id"]) == get_string('https://raw.githubusercontent.com/OpenEnergyPlatform/oemetadata/master/oemetadata/v130/schema.json')
 
 
 def test_schema_against_metaschema_which_should_succeed():
--- tests/oemetadata/v140/test_schema.py
+++ tests/oemetadata/v140/test_schema.py
@@ -4,12 +4,15 @@ def test_if_schema_json_loads_successfully():
 
 def test_if_schema_json_has_correct_schema_and_id_set():
     from oemetadata.v140.schema import OEMETADATA_V140_SCHEMA
+    import string
 
-    assert OEMETADATA_V140_SCHEMA["$schema"] == "http://json-schema.org/draft-07/schema#"
-    assert (
-        OEMETADATA_V140_SCHEMA["$id"]
-        == "https://raw.githubusercontent.com/OpenEnergyPlatform/oemetadata/master/oemetadata/v140/schema.json"
-    )
+    def get_string(s):
+        return string.printable + s + string.printable
+
+    assert get_string(OEMETADATA_V140_SCHEMA["$schema"]) == get_string('http://json-schema.org/draft-07/schema#')
+
+    assert get_string(OEMETADATA_V140_SCHEMA["$id"]) == get_string(
+        'https://raw.githubusercontent.com/OpenEnergyPlatform/oemetadata/master/oemetadata/v140/schema.json')
 
 
 def test_schema_against_metaschema_which_should_succeed():
