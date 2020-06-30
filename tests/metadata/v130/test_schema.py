f7c8592bb07d86a47624dc1a0b3daf5c469a396d
jh-RLI <jonas.huber@rl-institut.de>
2020-06-30 17:50:13 +0200

    refactor metadata folder name

--- tests/metadata/v130/test_schema.py
+++ tests/metadata/v130/test_schema.py
@@ -1,12 +1,12 @@
 def test_if_schema_json_loads_successfully():
     try:
-        from oemetadata.v130.schema import OEMETADATA_V130_SCHEMA
+        from metadata.v130.schema import OEMETADATA_V130_SCHEMA
     except Warning:
         print("Metadata Schema v1.3.0 cant load. Check if the files are missing!")
 
 
 def test_if_schema_json_has_correct_schema_and_id_set():
-    from oemetadata.v130.schema import OEMETADATA_V130_SCHEMA
+    from metadata.v130.schema import OEMETADATA_V130_SCHEMA
     import string
 
     def get_string(s):
@@ -21,7 +21,7 @@ def test_if_schema_json_has_correct_schema_and_id_set():
 
 def test_schema_against_metaschema_which_should_succeed():
     import jsonschema
-    from oemetadata.v130.schema import OEMETADATA_V130_SCHEMA
-    from oemetadata.metaschema.draft07.schema import OEMETADATA_METASCHEMA_DRAFT07_SCHEMA
+    from metadata.v130.schema import OEMETADATA_V130_SCHEMA
+    from metadata.metaschema.draft07.schema import OEMETADATA_METASCHEMA_DRAFT07_SCHEMA
 
     assert jsonschema.validate(OEMETADATA_V130_SCHEMA, OEMETADATA_METASCHEMA_DRAFT07_SCHEMA) is None
--- tests/metadata/v140/test_template.py
+++ tests/metadata/v140/test_template.py
@@ -1,10 +1,10 @@
 def test_if_template_json_loads_successfully():
-    from oemetadata.v140.template import OEMETADATA_V140_TEMPLATE
+    from metadata.v140.template import OEMETADATA_V140_TEMPLATE
 
 
 def test_template_against_schema_which_should_succeed():
     import jsonschema
-    from oemetadata.v140.template import OEMETADATA_V140_TEMPLATE
-    from oemetadata.v140.schema import OEMETADATA_V140_SCHEMA
+    from metadata.v140.template import OEMETADATA_V140_TEMPLATE
+    from metadata.v140.schema import OEMETADATA_V140_SCHEMA
 
     assert jsonschema.validate(OEMETADATA_V140_TEMPLATE, OEMETADATA_V140_SCHEMA) == None
