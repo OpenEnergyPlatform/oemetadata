81b7eee36b3378b851a280f0b132ae1ee99edae8
jh-RLI <jonas.huber@rl-institut.de>
2021-01-14 20:58:56 +0100

    merged release v1.4.1 into latest release folder

--- metadata/latest/example.py
+++ metadata/latest/example.py
@@ -2,4 +2,4 @@ import json
 import os
 
 with open(os.path.join(os.path.dirname(__file__), "example.json"), "rb") as f:
-    OEMETADATA_V140_EXAMPLE = json.loads(f.read())
+    OEMETADATA_V141_EXAMPLE = json.loads(f.read())
