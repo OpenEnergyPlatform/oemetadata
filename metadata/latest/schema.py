598bd066c2afaa373297ce579c6c55cd2882a24a
jh-RLI <jonas.huber@rl-institut.de>
2022-02-16 14:57:19 +0100

    refactor version names to latest in modules

--- metadata/latest/schema.py
+++ metadata/latest/schema.py
@@ -2,4 +2,4 @@ import json
 import os
 
 with open(os.path.join(os.path.dirname(__file__), "schema.json"), "rb") as f:
-    OEMETADATA_V150_SCHEMA = json.loads(f.read())
+    OEMETADATA_LATEST_SCHEMA = json.loads(f.read())
