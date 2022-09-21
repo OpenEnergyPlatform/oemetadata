733cc0ccd6e81a092ba0032453b2230ca1c26339
jh-RLI <jonas.huber@rl-institut.de>
2022-09-21 11:39:43 +0200

    raise oemetadata version to be able to test pypi(test) release with json files)

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="oemetadata",
-    version="1.5.1",
+    version="1.5.1a1",
     description="Open Energy Platform (OEP) - metadata schemas, examples and templates package",
     long_description=long_description,
     long_description_content_type="text/markdown",
