6761763a58ba0e5809f89b3beb051cc2160042d0
Ludwig Hülk <ludwig.huelk@rl-institut.de>
2024-11-06 11:58:27 +0100

    Rename json schema folder #210

--- metadata/json_schema/draft07/schema.py
+++ metadata/json_schema/draft07/schema.py
@@ -2,4 +2,4 @@ import json
 import os
 
 with open(os.path.join(os.path.dirname(__file__), "schema.json"), "rb") as f:
-    OEMETADATA_METASCHEMA_DRAFT07_SCHEMA = json.loads(f.read())
+    OEMETADATA_JSONSCHEMA_DRAFT07_SCHEMA = json.loads(f.read())
