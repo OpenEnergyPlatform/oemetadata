b39c65ffe081ffc91f37a526344a70722da81cbd
Ludwig Hülk <ludwig.huelk@rl-institut.de>
2024-11-06 14:07:28 +0100

    Rename and restructure json schema tests #210

---
+++ tests/metadata/json_schema/draft2020_12/test_jsonschema.py
@@ -0,0 +1,19 @@
+def test_jsonschema_should_load():
+    try:
+        from metadata.json_schema.draft2020_12.schema \
+            import OEMETADATA_JSONSCHEMA_DRAFT202012_SCHEMA
+    except Warning:
+        print("Cannot open JSON Schema (draft2020-12)!")
+
+
+def test_jsonschema_should_have_correct_path():
+    from metadata.json_schema.draft2020_12.schema \
+        import OEMETADATA_JSONSCHEMA_DRAFT202012_SCHEMA
+
+    assert OEMETADATA_JSONSCHEMA_DRAFT202012_SCHEMA["$schema"] \
+        == "https://json-schema.org/draft/2020-12/schema#", \
+        "Wrong schema path in JSON Schema (draft2020-12)!"
+
+    assert OEMETADATA_JSONSCHEMA_DRAFT202012_SCHEMA["$id"] \
+        == "https://json-schema.org/draft/2020-12/schema#", \
+        "Wrong id path in JSON Schema (draft2020-12)!"
