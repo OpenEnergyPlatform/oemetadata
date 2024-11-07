e9797e41b61c9cad951e4ba6f39bb9862fe01e94
Ludwig Hülk <ludwig.huelk@rl-institut.de>
2024-11-07 11:31:05 +0100

    Update JSON schema for v20 #210

--- tests/metadata/v20/v20/test_schema.py
+++ tests/metadata/v20/v20/test_schema.py
@@ -8,12 +8,12 @@ def test_oemetadata_schema_should_load():
 def test_jsonschema_should_validate_oemetadata_schema():
     from jsonschema import validate, ValidationError
     from metadata.v20.v20.schema import OEMETADATA_V20_SCHEMA
-    from metadata.json_schema.draft07.schema \
-        import OEMETADATA_JSONSCHEMA_DRAFT07_SCHEMA
+    from metadata.json_schema.draft2020_12.schema \
+        import OEMETADATA_JSONSCHEMA_DRAFT202012_SCHEMA
 
     try:
-        validate(OEMETADATA_V20_SCHEMA, OEMETADATA_JSONSCHEMA_DRAFT07_SCHEMA)
-        print("OEMetadata Schema (v2.0) is valid JSON Schema.")
+        validate(OEMETADATA_V20_SCHEMA, OEMETADATA_JSONSCHEMA_DRAFT202012_SCHEMA)
+        print("OEMetadata Schema (v2.0) is valid JSON Schema (Draft 2020-12).")
     except ValidationError as e:
         print("Cannot validate OEMetadata Schema with JSON Schema (v2.0)!", e)
 
@@ -26,7 +26,7 @@ def test_oemetadata_schema_should_have_correct_path():
         return string.printable + s + string.printable
 
     assert get_string(OEMETADATA_V20_SCHEMA["$schema"]) == get_string(
-        "http://json-schema.org/draft-07/schema#"
+        "https://json-schema.org/draft/2020-12/schema"
     ), "Wrong schema path in OEMetadata Schema (v2.0)!"
 
     assert get_string(OEMETADATA_V20_SCHEMA["$id"]) == get_string(
