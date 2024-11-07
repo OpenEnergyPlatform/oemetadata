5fdaffc93960d1e6e82f7e789a63d7df794bdba4
Ludwig Hülk <ludwig.huelk@rl-institut.de>
2024-11-07 11:23:33 +0100

    Update JSON schema test #210

--- tests/metadata/json_schema/draft2020_12/test_jsonschema.py
+++ tests/metadata/json_schema/draft2020_12/test_jsonschema.py
@@ -6,20 +6,20 @@ def test_jsonschema_should_load():
         print("Cannot open JSON Schema (draft2020-12)!")
 
 
-def test_jsonschema_should_have_correct_path_old():
+def test_jsonschema_should_have_correct_path():
     from metadata.json_schema.draft2020_12.schema \
         import OEMETADATA_JSONSCHEMA_DRAFT202012_SCHEMA
 
     assert OEMETADATA_JSONSCHEMA_DRAFT202012_SCHEMA["$schema"] \
         == "https://json-schema.org/draft/2020-12/schema", \
-        "Wrong schema path in JSON Schema (draft2020-12) old!"
+        "Wrong schema path in JSON Schema (draft2020-12)!"
 
     assert OEMETADATA_JSONSCHEMA_DRAFT202012_SCHEMA["$id"] \
         == "https://json-schema.org/draft/2020-12/schema", \
-        "Wrong id path in JSON Schema (draft2020-12) old!"
+        "Wrong id path in JSON Schema (draft2020-12)!"
 
 
-def test_jsonschema_should_have_correct_path():
+def test_jsonschema_should_have_correct_path_string():
     from metadata.json_schema.draft2020_12.schema \
         import OEMETADATA_JSONSCHEMA_DRAFT202012_SCHEMA
     import string
@@ -28,9 +28,9 @@ def test_jsonschema_should_have_correct_path():
         return string.printable + s + string.printable
 
     assert get_string(OEMETADATA_JSONSCHEMA_DRAFT202012_SCHEMA["$schema"]) \
-        == get_string("https://json-schema.org/draft/2020-12/schema"), \
-        "Wrong schema path in JSON Schema (draft2020-12)!"
+        is get_string("https://json-schema.org/draft/2020-12/schema"), \
+        "Wrong schema path in JSON Schema (draft2020-12) with strings!"
 
     assert get_string(OEMETADATA_JSONSCHEMA_DRAFT202012_SCHEMA["$id"]) \
-        == get_string("https://json-schema.org/draft/2020-12/schema"), \
-        "Wrong id path in JSON Schema (draft2020-12)!"
+        is get_string("https://json-schema.org/draft/2020-12/schema"), \
+        "Wrong id path in JSON Schema (draft2020-12) with strings!"
