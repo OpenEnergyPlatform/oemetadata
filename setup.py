88fcfaf038eed21572e3814f4bfda91e88351a1f
Ludwig Hülk <ludwig.huelk@rl-institut.de>
2025-01-24 11:41:26 +0100

    Update version number #226

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, "README.rst"), encoding="utf-8") as f:
 
 setup(
     name="oemetadata",
-    version="2.0.1",
+    version="2.0.2",
     description="Open Energy Metadata (OEMetadata) - The energy metadata standard",
     long_description=long_description,
     long_description_content_type="text/x-rst",
