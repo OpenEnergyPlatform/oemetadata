839bcfa6ec30635b642e78cee9d37d5a5b68488e
Ludwig Hülk <ludwig.huelk@rl-institut.de>
2025-01-28 00:37:44 +0100

    Add inits #250

---
+++ metadata/v2/__init__.py
@@ -0,0 +1,8 @@
+"""Example functions
+
+Examples calculater functionality.
+
+SPDX-FileCopyrightText: 2022 Jonas Huber <https://github.com/jh-rli> © Reiner Lemoine Institut
+SPDX-FileCopyrightText: 2022 Ludwig Hülk <https://github.com/Ludee> © Reiner Lemoine Institut
+SPDX-License-Identifier: MIT
+"""
