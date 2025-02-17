09b29895b95e7e710f0e2c381bfe71869ea8a429
Ludwig Hülk <ludwig.huelk@rl-institut.de>
2025-02-17 14:22:15 +0100

    Adapt latest folder and run scripts #151

--- oemetadata/latest/build_source/scripts/settings.py
+++ oemetadata/latest/build_source/scripts/settings.py
@@ -8,8 +8,8 @@ from pathlib import Path
 
 LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 
-BASE_PATH = Path("oemetadata/v2/")
-VERSION = "v20"
+BASE_PATH = Path("oemetadata/")
+VERSION = "latest"
 VERSION_PATH = BASE_PATH / VERSION
 SCHEMA_BUILD_PATH = VERSION_PATH / "build_source"
 MAIN_SCHEMA_PATH = SCHEMA_BUILD_PATH / "schema_structure.json"
