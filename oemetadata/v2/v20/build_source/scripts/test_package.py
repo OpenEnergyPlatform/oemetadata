63464330c011afa8e485c45f68a8161b326594c6
Ludwig Hülk <ludwig.huelk@rl-institut.de>
2025-02-19 21:24:33 +0100

    Add script to test import #263

---
+++ oemetadata/v2/v20/build_source/scripts/test_package.py
@@ -0,0 +1,20 @@
+# SPDX-FileCopyrightText: 2025 Ludwig Hülk <@Ludee> © Reiner Lemoine Institut
+# SPDX-License-Identifier: MIT
+
+from oemetadata.latest.schema import OEMETADATA_LATEST_SCHEMA
+from oemetadata.latest.example import OEMETADATA_LATEST_EXAMPLE
+from oemetadata.latest.template import OEMETADATA_LATEST_TEMPLATE
+
+# Print the schema
+print("Schema:")
+print(OEMETADATA_LATEST_SCHEMA)
+print("\n" + "="*50 + "\n")
+
+# Print the example
+print("Example:")
+print(OEMETADATA_LATEST_EXAMPLE)
+print("\n" + "="*50 + "\n")
+
+# Print the template
+print("Template:")
+print(OEMETADATA_LATEST_TEMPLATE)
