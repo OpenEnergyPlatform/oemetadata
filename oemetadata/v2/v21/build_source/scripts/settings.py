a04d5ef956cb6030d1cfe58a8babb87568677541
Ludwig Hülk <ludwig.huelk@rl-institut.de>
2026-02-24 23:32:25 +0100

    Cherry-pick version start #293

---
+++ oemetadata/v2/v21/build_source/scripts/settings.py
@@ -0,0 +1,24 @@
+# SPDX-FileCopyrightText: 2024 Ludwig Hülk <@Ludee> © Reiner Lemoine Institut
+# SPDX-FileCopyrightText: 2024 Jonas Huber <jh-RLI> © Reiner Lemoine Institut
+# SPDX-FileCopyrightText: oemetadata <https://github.com/OpenEnergyPlatform/oemetadata/>
+# SPDX-License-Identifier: MIT
+
+from pathlib import Path
+
+
+LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+
+BASE_PATH = Path("oemetadata/v2/")
+VERSION = "v20"
+VERSION_PATH = BASE_PATH / VERSION
+SCHEMA_BUILD_PATH = VERSION_PATH / "build_source"
+MAIN_SCHEMA_PATH = SCHEMA_BUILD_PATH / "schema_structure.json"
+SCHEMA_REFS = SCHEMA_BUILD_PATH / "schemas"
+SCHEMA_EXAMPLE_FIELDS = SCHEMA_BUILD_PATH / "scripts/example/fields.json"
+SCHEMA_EXAMPLE_PROV = SCHEMA_BUILD_PATH / "scripts/example/contributors.json"
+RESOLVED_SCHEMA_FILE_NAME = VERSION_PATH / "schema.json"
+EXPECTED_SCHEMA_PATH = VERSION_PATH / "schema.json"
+
+EXAMPLE_PATH = VERSION_PATH / "example.json"
+TEMPLATE_PATH = VERSION_PATH / "template.json"
+LATEST_PATH = Path("oemetadata/latest/")
