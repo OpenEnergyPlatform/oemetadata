e225d732c7a13006fca2907b06f30b297a93ec15
Ludwig Hülk <ludwig.huelk@rl-institut.de>
2026-04-09 00:57:27 +0200

    Add frictionless validate tests #290 #296

---
+++ test/oemetadata/v2/v21/test_example.py
@@ -0,0 +1,33 @@
+# SPDX-FileCopyrightText: 2026 Jonas Huber <jh-RLI> © Reiner Lemoine Institut
+# SPDX-FileCopyrightText: 2026 Ludwig Hülk <@Ludee> © Reiner Lemoine Institut
+# SPDX-FileCopyrightText: oemetadata <https://github.com/OpenEnergyPlatform/oemetadata/>
+# SPDX-License-Identifier: MIT
+
+
+def test_oemetadata_example_should_load():
+    try:
+        pass
+    except Warning:
+        print("Cannot open OEMetadata Example (v2.1)!")
+
+
+def test_oemetadata_schema_should_validate_oemetadata_example():
+    from jsonschema import ValidationError, validate
+
+    from oemetadata.v2.v21.example import OEMETADATA_V21_EXAMPLE
+    from oemetadata.v2.v21.schema import OEMETADATA_V21_SCHEMA
+
+    try:
+        validate(OEMETADATA_V21_EXAMPLE, OEMETADATA_V21_SCHEMA)
+        print("OEMetadata Example is valid OEMetadata Schema (v2.1).")
+    except ValidationError as e:
+        print("Cannot validate OEMetadata Example with Schema (v2.1)!", e)
+
+
+def test_oemetadata_example_is_datapackage():
+    from frictionless import validate
+
+    from oemetadata.v2.v21.example import OEMETADATA_V21_EXAMPLE
+
+    report = validate(OEMETADATA_V21_EXAMPLE)
+    assert report.valid, report.flatten(["message"])
---
+++ test/oemetadata/v2/v21/test_template.py
@@ -0,0 +1,33 @@
+# SPDX-FileCopyrightText: 2026 Jonas Huber <jh-RLI> © Reiner Lemoine Institut
+# SPDX-FileCopyrightText: 2026 Ludwig Hülk <@Ludee> © Reiner Lemoine Institut
+# SPDX-FileCopyrightText: oemetadata <https://github.com/OpenEnergyPlatform/oemetadata/>
+# SPDX-License-Identifier: MIT
+
+
+def test_oemetadata_template_should_load():
+    try:
+        pass
+    except Warning:
+        print("Cannot open OEMetadata Template (v2.1)!")
+
+
+def test_oemetadata_schema_should_validate_oemetadata_template():
+    from jsonschema import ValidationError, validate
+
+    from oemetadata.v2.v21.schema import OEMETADATA_V21_SCHEMA
+    from oemetadata.v2.v21.template import OEMETADATA_V21_TEMPLATE
+
+    try:
+        validate(OEMETADATA_V21_TEMPLATE, OEMETADATA_V21_SCHEMA)
+        print("OEMetadata Template is valid OEMetadata Schema (v2.1).")
+    except ValidationError as e:
+        print("Cannot validate OEMetadata Template with Schema (v2.1)!", e)
+
+
+def test_oemetadata_template_is_datapackage():
+    from frictionless import validate
+
+    from oemetadata.v2.v21.template import OEMETADATA_V21_TEMPLATE
+
+    report = validate(OEMETADATA_V21_TEMPLATE)
+    assert report.valid, report.flatten(["message"])
