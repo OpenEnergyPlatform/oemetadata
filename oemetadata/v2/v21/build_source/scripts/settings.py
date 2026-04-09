7efa942694cf45320ef85ab9f1da207c29b3fd33
Ludwig Hülk <ludwig.huelk@rl-institut.de>
2026-04-09 14:34:25 +0200

    Update example #290

--- oemetadata/v2/v21/build_source/scripts/settings.py
+++ oemetadata/v2/v21/build_source/scripts/settings.py
@@ -9,7 +9,7 @@ from pathlib import Path
 LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 
 BASE_PATH = Path("oemetadata/v2/")
-VERSION = "v20"
+VERSION = "v21"
 VERSION_PATH = BASE_PATH / VERSION
 SCHEMA_BUILD_PATH = VERSION_PATH / "build_source"
 MAIN_SCHEMA_PATH = SCHEMA_BUILD_PATH / "schema_structure.json"
