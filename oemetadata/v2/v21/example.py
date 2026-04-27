89ef56dc3c36adc885b6f9fd11354dfdfc0e1f8d
Ludwig Hülk <ludwig.huelk@rl-institut.de>
2026-04-27 14:35:00 +0200

    Update example package #290

--- oemetadata/v2/v21/example.py
+++ oemetadata/v2/v21/example.py
@@ -8,4 +8,4 @@ import os
 
 
 with open(os.path.join(os.path.dirname(__file__), "example.json"), "rb") as f:
-    OEMETADATA_V21_SCHEMA = json.loads(f.read())
+    OEMETADATA_V21_EXAMPLE = json.loads(f.read())
